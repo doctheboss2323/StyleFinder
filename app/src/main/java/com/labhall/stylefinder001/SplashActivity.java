48c80e0d14bbae8ce673302e48db635327d30e64
elieh <elie.hallermeier@gmail.com>
2023-06-24 16:21:58 +0300

    0.0.1 Splash screen

---
+++ app/src/main/java/com/labhall/stylefinder001/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.labhall.stylefinder001;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView icon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        icon=findViewById(R.id.icon);
+        if (getSupportActionBar() != null) {  //hide action bar
+            getSupportActionBar().hide();}
+        rotateImg();
+        new CountDownTimer(3000, 1000   ) {
+            public void onTick(long millisUntilFinished) {
+            }
+            public void onFinish() {
+//                Intent intent =new Intent(SplashActivity.this,gameActivity.class);
+//                startActivity(intent);
+            }
+        }.start();
+
+    }
+    public void rotateImg(){
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        rotateAnimation.setDuration(5000);
+        rotateAnimation.setRepeatCount(Animation.INFINITE);
+        findViewById(R.id.icon).startAnimation(rotateAnimation);
+    }
+}
