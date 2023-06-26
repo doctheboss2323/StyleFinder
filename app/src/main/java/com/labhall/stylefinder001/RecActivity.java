e60163f0c3d792e64dde283b7d3b25cd40d0b0ae
elieh <elie.hallermeier@gmail.com>
2023-06-26 17:34:08 +0300

    0.1.0 All screens rec working no img img in res not drawable

--- app/src/main/java/com/labhall/stylefinder001/RecActivity.java
+++ app/src/main/java/com/labhall/stylefinder001/RecActivity.java
@@ -5,24 +5,21 @@ import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.content.res.AssetManager;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 
 import com.labhall.stylefinder001.ui.Store;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
-
 
 
 public class RecActivity extends AppCompatActivity {
@@ -38,7 +35,7 @@ public class RecActivity extends AppCompatActivity {
 
         String selectedStyle="";
         String selectedPrice="";
-        Store[] stores = new Store [10];
+        Store[] stores = new Store[10];
 
         container = findViewById(R.id.container);
 
@@ -48,31 +45,42 @@ public class RecActivity extends AppCompatActivity {
             selectedPrice = intent.getStringExtra("selectedPrice");
         }
 
+
+
+
         try {
             AssetManager assetManager = getAssets();
             InputStream inputStream = assetManager.open("stores_list.csv");
-            CSVReader reader = new CSVReader(new InputStreamReader(inputStream));
 
-            String[] nextLine;
-            for (int i = 0; i < stores.length; i++) {
-                nextLine = reader.readNext();
-                Store store= new Store(nextLine[0],nextLine[1],Integer.valueOf(nextLine[2]),nextLine[3]);
-                stores[i]=store;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                // Process each line of the CSV file
+                String[] data = line.split(","); // Assuming comma-separated values
+
+                LinearLayout storeLayout = createStoreLayoutNoImg(data[0],data[1],data[3]);
+                container.addView(storeLayout);
             }
 
+            // Close the reader after reading the file
             reader.close();
-        } catch (IOException | CsvValidationException e) {
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        stores=addImgs(stores);
-        stores=giveScores(stores,selectedStyle,selectedPrice);
-        stores=sortStores(stores);
 
-        for (Store store : stores) {
-            LinearLayout storeLayout = createStoreLayout(store);
-            container.addView(storeLayout);
-        }
+
+//        stores=addImgs(stores);
+//        stores=giveScores(stores,selectedStyle,selectedPrice);
+//        stores=sortStores(stores);
+//
+//        for (Store store : stores) {
+//            LinearLayout storeLayout = createStoreLayout(store);
+//            container.addView(storeLayout);
+//        }
+
 
         //now show the stores according to score
         //then keep preferences per user
@@ -114,44 +122,49 @@ public class RecActivity extends AppCompatActivity {
     }
 
     private int[] getImagesFromFolder(String folderName) {
-        ArrayList<Integer> imageList = new ArrayList<>();
-        Resources res = getResources();
+        AssetManager assetManager = getAssets();
+        String[] imagePaths;
+        int[] imageResources = new int[5];
 
-        // Get the identifier of the folder
-        int folderId = res.getIdentifier(folderName, "drawable", getPackageName());
+        try {
+            imagePaths = assetManager.list(folderName);
 
-        // Get the list of all resources in the folder
-        TypedArray typedArray = res.obtainTypedArray(folderId);
-
-        for (int i = 0; i < typedArray.length(); i++) {
-            int resourceId = typedArray.getResourceId(i, 0);
-            imageList.add(resourceId);
+            if (imagePaths != null && imagePaths.length >= 5) {  //Later change it back to 5
+                for (int i = 0; i < 4; i++) { //here from 4 to 5 and uncomment the lower version
+                    String imagePath = "images/" + folderName + "/" + imagePaths[i];
+                    InputStream inputStream = assetManager.open(imagePath);
+                    Drawable drawable = Drawable.createFromStream(inputStream, null);
+                    imageResources[i] = getResources().getIdentifier(imagePath, null, getPackageName());
+                }
+            }
+//            if (imagePaths != null && imagePaths.length == 4) {  //  because flashback has 4
+//                for (int i = 0; i < 4; i++) {
+//                    String imagePath = "images/" + folderName + "/" + imagePaths[i];
+//                    InputStream inputStream = assetManager.open(imagePath);
+//                    Drawable drawable = Drawable.createFromStream(inputStream, null);
+//                    imageResources[i] = getResources().getIdentifier(imagePath, null, getPackageName());
+//                }
+//                imageResources[4]=0;
+//            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        typedArray.recycle();
-
-        // Convert the ArrayList to an array
-        int[] imageArray = new int[5];
-        for (int i = 0; i < imageList.size(); i++) {
-            if(imageList.get(i)!=null){
-                imageArray[i] = imageList.get(i);}
-            else{
-                imageArray[i]=0;}
-        }
-        return imageArray;
+        return imageResources;
     }
 
+
     public Store[] addImgs(Store[] stores){
-        stores[0].setImages(getImagesFromFolder("Flashback"));
-        stores[1].setImages(getImagesFromFolder("hayo_haya"));
-        stores[2].setImages(getImagesFromFolder("zara"));
-        stores[3].setImages(getImagesFromFolder("bitch_please"));
-        stores[4].setImages(getImagesFromFolder("bolenat"));
-        stores[5].setImages(getImagesFromFolder("ruby_bay"));
-        stores[6].setImages(getImagesFromFolder("hm"));
-        stores[7].setImages(getImagesFromFolder("jiffa"));
-        stores[8].setImages(getImagesFromFolder("street_dolls"));
-        stores[9].setImages(getImagesFromFolder("studio_pasha"));
+        stores[0].setImages(getImagesFromFolder("images/Flashback"));
+        stores[1].setImages(getImagesFromFolder("images/hayo_haya"));
+        stores[2].setImages(getImagesFromFolder("images/zara"));
+        stores[3].setImages(getImagesFromFolder("images/bitch_please"));
+        stores[4].setImages(getImagesFromFolder("images/bolenat"));
+        stores[5].setImages(getImagesFromFolder("images/ruby_bay"));
+        stores[6].setImages(getImagesFromFolder("images/hm"));
+        stores[7].setImages(getImagesFromFolder("images/jiffa"));
+        stores[8].setImages(getImagesFromFolder("images/street_dolls"));
+        stores[9].setImages(getImagesFromFolder("images/studio_pasha"));
         return stores;
     }
 
@@ -168,25 +181,26 @@ public class RecActivity extends AppCompatActivity {
 
         TextView priceTextView = new TextView(this);
         priceTextView.setText("Price: " + store.getPrice());
+        // add adress
 
         // Create an ImageView for each image in the store's images array
-        int[] images = store.getImages();
-        for (int imageResId : images) {
-            if(imageResId!=0){
-                ImageView imageView = new ImageView(this);
-                imageView.setImageResource(imageResId);
-                imageView.setAdjustViewBounds(true);
-                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT
-                );
-                layoutParams.setMargins(0, 16, 0, 16);
-
-                storeLayout.addView(imageView, layoutParams);
-        }
-        }
+//        int[] images = store.getImages();
+//        for (int imageResId : images) {
+//            if(imageResId!=0){
+//                ImageView imageView = new ImageView(this);
+//                imageView.setImageResource(imageResId);
+//                imageView.setAdjustViewBounds(true);
+//                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+//
+//                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+//                        LinearLayout.LayoutParams.MATCH_PARENT,
+//                        LinearLayout.LayoutParams.WRAP_CONTENT
+//                );
+//                layoutParams.setMargins(0, 16, 0, 16);
+//
+//                storeLayout.addView(imageView, layoutParams);
+//        }
+//        }
 
         storeLayout.addView(nameTextView);
         storeLayout.addView(styleTextView);
@@ -203,5 +217,36 @@ public class RecActivity extends AppCompatActivity {
 
         return storeLayout;
     }
+
+    private LinearLayout createStoreLayoutNoImg(String s1,String s2,String s3) {
+        LinearLayout storeLayout = new LinearLayout(this);
+        storeLayout.setOrientation(LinearLayout.VERTICAL);
+        storeLayout.setPadding(16, 16, 16, 16);
+
+        TextView nameTextView = new TextView(this);
+        nameTextView.setText("Name: " + s1);
+
+        TextView styleTextView = new TextView(this);
+        styleTextView.setText("Style: " + s2);
+
+        TextView priceTextView = new TextView(this);
+        priceTextView.setText("Price: " + s3);
+
+        storeLayout.addView(nameTextView);
+        storeLayout.addView(styleTextView);
+        storeLayout.addView(priceTextView);
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(0, 0, 0, 24);
+
+        storeLayout.setLayoutParams(layoutParams);
+        storeLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.pink_nude));
+
+        return storeLayout;
+    }
+
 
 }
